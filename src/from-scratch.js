e217ac2fc828e86514626c6a7fde60eb76816a5e
Mei Zhu <maggiez0613@gmail.com>
2024-04-11 15:05:36 -0400

    up to q4 & q6 done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,22 +1,42 @@
-const coolGreeting = () => {
+const coolGreeting = (person) => {
+  if(person.isCool === true) {
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`;
+  } else {
+    return `Greetings ${person.name}, how have you been lately?`;
+  }
 };
 
-const haveBirthday = () => {
+const haveBirthday = (person) => {
+  const {name, age} = person;
+  person.age += 1;
 };
 
-const becomeSecretAgent = () => {
+const becomeSecretAgent = (person, spyHandle) => {
+  delete person.name; 
+  person.spyHandle = spyHandle; 
 };
 
-const carMaker = () => {
+const carMaker = (name, maker, year) => {
+  return {
+    name: name,
+    year: year,
+    maker: maker,
+    needsOilChange: false,
+  }
 };
 
 const weAreNotFriends = () => {
 };
 
-const listHobbies = () => {
+const listHobbies = (person) => {
+  for (let i = 0; i < person.hobbies.length; i++) {
+    console.log(`${person.name} likes ${person.hobbies[i]}.`);
+  }
 };
 
-const getNextOpponent = () => {
+const getNextOpponent = (team) => {
+  // return team.matches[0][0];
+  team.matches.teamName(0);
 };
 
 const listAllKeys = () => {
